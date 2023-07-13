abdcdc7376188440ff2ff285d80235ab1a7530e2
Waleed Lateef <waleedlateef@Waleeds-MacBook-Pro.local>
2023-07-13 12:26:47 +0500

    some new concept

--- polls/urls.py
+++ polls/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/newchoice/", views.newchoice, name= "newchoice"),
+    path("<int:question_id>/vote_reset/", views.vote_reset, name="vote_reset"),
+    path("new/", views.add_question, name="new"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import  HttpResponseRedirect
+from django.http import  HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -24,7 +25,6 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -46,3 +46,53 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+def newchoice(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+       new_choice=request.POST["new_choice"]
+       if not new_choice:
+           raise KeyError
+    except (KeyError):
+        return render(
+           request,
+           "polls/addchoice.html",
+           {
+              "question":question,
+              "error_message": "choice could not be added",
+           },
+        
+        )
+    else:
+        newchoice=Choice(
+          question=question,
+          choice_text=new_choice
+     )
+        newchoice.save()
+        return HttpResponseRedirect(reverse("polls:detail",args=(question.id,)))
+    
+def vote_reset(request, question_id):
+     question = get_object_or_404(Question, pk=question_id)
+
+     for choice in question.choice_set.all():
+         choice.votes = 0
+         choice.save()
+
+     return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def add_question(request):
+     if request.method == 'GET':
+         return render(request, "polls/new_question.html", {})
+     elif request.method == 'POST':
+         user_submitted_question = request.POST["question"]
+         if not user_submitted_question:
+             return render(request, "polls/new_question.html", {
+                 "error_message": "Please enter a valid question"
+             })
+
+         new_question = Question(
+             question_text=user_submitted_question,
+             pub_date=timezone.now(),
+         )
+         new_question.save()
+     return HttpResponseRedirect(reverse("polls:index",))
